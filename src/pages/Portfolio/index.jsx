dcfc265491fe0173391863e597c46a8e73f5e13f
Narek-Martirosyan <narekmartirosyan583@gmail.com>
2022-07-13 11:54:00 +0400

    try to add media for iOS

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -4,7 +4,13 @@ import tictactoe from './img/tictactoe.png';
 export default function Portfolio() {
     return (
         <div>
-            <h1>Portfolio page</h1>
+            
+            <Card
+                img={tictactoe}
+                title={'Tic Tac Toe'}
+                text={'The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner'}
+                url={'/portfolio/tictactoe'}
+            />
             <Card
                 img={tictactoe}
                 title={'Tic Tac Toe'}
